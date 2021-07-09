c8dba4e2ef53afcdc081e31ab230ddec02dc3b9a
Ivan Mamani <ivan.mamani.q@ucb.edu.bo>
2021-07-09 12:40:49 -0400

    Keycloak

---
+++ src/app/app.module.ts
@@ -0,0 +1,47 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+
+import { OAuthModule } from 'angular-oauth2-oidc';
+import { HomeComponent } from './home/home.component';
+import { MenuComponent } from './menu/menu.component';
+import { ListaComponent } from './foo/lista/lista.component';
+import { DetailComponent } from './foo/detail/detail.component';
+import { CreateComponent } from './foo/create/create.component';
+import { UpdateComponent } from './foo/update/update.component';
+import { FooComponent } from './services/foo/foo.component';
+import { SignupComponent } from './signup/signup.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    MenuComponent,
+    ListaComponent,
+    DetailComponent,
+    CreateComponent,
+    UpdateComponent,
+    FooComponent,
+    SignupComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    OAuthModule.forRoot({
+      resourceServer: {
+        allowedUrls: ['http://localhost:8080/foo'],
+        sendAccessToken: true
+      }
+    })
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import {MessageService} from "../services/message.service";
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+  username: string='';
+  //username: string;
+
+  constructor(private messageService: MessageService) { }
+
+  ngOnInit(): void {
+    this.messageService.getMessage().subscribe( res => {
+        this.username = res[`text`];
+      },
+      err => console.log(err));
+  }
+
+}
